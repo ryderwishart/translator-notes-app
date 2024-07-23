957d74ef763cd9108e1a3f990bf6c855173d7e66
ryderwishart <ryderwishart@gmail.com>
2024-07-23 14:35:59 -0600

    Use custom sqlite3 binding

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,11 @@ from llm_interface import LLMInterface
 from typing import List, Union
 import os
 
+# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 app = FastAPI()
 
 # Initialize components
